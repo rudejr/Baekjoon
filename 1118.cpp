a6261467620c51179bc2a6134503daec9d93d234
g_de0k <akg13078@naver.com>
2023-11-18 12:32:20 +0900

    BronzeIII 2442

---
+++ 1118.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+	int n;
+
+	while ( 1 ) {
+
+		scanf("%d", &n);
+		while (getchar() != '\n');
+
+		if (n > 0 && n <= 100) break;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j < n - i; j++) {
+			printf(" ");
+		}
+		for (int j = 0; j < (2 * i) - 1; j++) {
+			printf("*");
+		} printf("\n");
+	}
+
+	return 0;
+}
