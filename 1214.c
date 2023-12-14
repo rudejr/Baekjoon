3ea19a98458b8e67c7e6e678158dde3bb3ab66c3
g_de0k <akg13078@naver.com>
2023-12-14 15:35:41 +0900

    BronzeV 10872

---
+++ 1214.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+long long factorial(int n);
+
+int main() {
+    int n;
+    long long result;
+
+    while (1) {
+        scanf("%d", &n);
+        if (n >= 0 && n <= 12) break;
+    } result = factorial(n);
+
+    printf("%lld\n", result);
+
+    return 0;
+}
+
+long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
